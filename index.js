106d6403c85beed918a2989d0c8b580384430ddb
Timothy Harper <tharper7@student.cvtc.edu>
2022-04-06 14:46:08 -0500

    Add /db-info route with table display.

--- index.js
+++ index.js
@@ -28,4 +28,23 @@ express()
       res.send("Error " + err);
     }
 })
+.get('/db-info', async(req, res) => {
+  try {
+    const client = await pool.connect();
+    const tables = await client.query(
+"SELECT c.relname AS table, a.attname AS column, t.typname AS type FROM pg_catalog.pg_class AS c LEFT JOIN pg_catalog.pg_attribute AS a ON c.oid = a.attrelid AND a.attnum > 0 LEFT JOIN pg_catalog.pg_type AS t ON a.atttypid = t.oid WHERE c.relname IN ('users', 'observations', 'students', 'schools', 'tasks') ORDER  BY c.relname, a.attnum;"
+);
+    
+    const locals = {
+      'tables': (tables) ? tables.rows : null
+    };
+    
+    res.render('pages/db-info', locals);
+    client.release();
+  }
+  catch (err) {
+    console.error(err);
+    res.send("Error: " + err);
+  }
+})
 .listen(PORT, () => console.log('Listening on ${ PORT }'));
---
+++ support/schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  email text NOT NULL,
+  password text NOT NULL
+);
+
+CREATE TABLE students (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  schol INT NOT NULL,
+  expires DATE NOT NULL
+);
+
+CREATE TABLE schools (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  address TEXT NOT NULL
+);
+
+CREATE TABLE observations (
+  id SERIAL PRIMARY KEY,
+  users_id INT NOT NULL,
+  students_id INT NOT NULL,
+  tasks_id INT NOT NULL,
+  duration INTERVAL NOT NULL
+);
+
+CREATE TABLE tasks (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
