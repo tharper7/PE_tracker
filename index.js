fca0084cece716deec33fcf0476534a0e886d1e3
Timothy Harper <tharper7@student.cvtc.edu>
2022-04-05 20:51:10 -0500

    Fixed line 6 on index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-const { POOL } = require('pg');
+const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
