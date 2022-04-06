48ad8d37b69b572523d6aced273c3a59d1d6e9c9
Timothy Harper <tharper7@student.cvtc.edu>
2022-04-06 14:58:55 -0500

    Temporary support for creating tables.

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ express()
     const client = await pool.connect();
 
     const tables = await client.query(
-"
+'
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   email text NOT NULL,
@@ -66,7 +66,7 @@ CREATE TABLE tasks (
   name TEXT NOT NULL
 );      
       
-SELECT c.relname AS table, a.attname AS column, t.typname AS type FROM pg_catalog.pg_class AS c LEFT JOIN pg_catalog.pg_attribute AS a ON c.oid = a.attrelid AND a.attnum > 0 LEFT JOIN pg_catalog.pg_type AS t ON a.atttypid = t.oid WHERE c.relname IN ('users', 'observations', 'students', 'schools', 'tasks') ORDER  BY c.relname, a.attnum;"
+SELECT c.relname AS table, a.attname AS column, t.typname AS type FROM pg_catalog.pg_class AS c LEFT JOIN pg_catalog.pg_attribute AS a ON c.oid = a.attrelid AND a.attnum > 0 LEFT JOIN pg_catalog.pg_type AS t ON a.atttypid = t.oid WHERE c.relname IN ('users', 'observations', 'students', 'schools', 'tasks') ORDER  BY c.relname, a.attnum;'
 );
     
     const locals = {
